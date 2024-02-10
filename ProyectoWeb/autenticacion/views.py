400ac603da689bf7355415bd820101c77a9e6302
Katherin Ambia Payano <katherin_carina06@hotmail.com>
2024-02-10 02:15:52 +0100

    se implementa funcionalidad para agregar usuario

--- ProyectoWeb/autenticacion/views.py
+++ ProyectoWeb/autenticacion/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate 
+from django.contrib import messages
+
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 # Create your views here.
 
 
@@ -9,5 +12,20 @@ class VRegistro(View):
     def get(self, request):
         form=UserCreationForm()
         return render(request, "autenticacion/autenticacion.html", {"form":form})
+    
     def post(self, request):
-        pass
+        form=UserCreationForm(request.POST)
+
+        if form.is_valid():
+
+            usuario=form.save()
+
+            login(request, usuario)
+
+            return redirect('Contacto')
+
+        else:
+            for msg in form.error_messages:
+                messages.error(request, form.error_messages[msg])
+
+            return render(request,"autenticacion/autenticacion.html",{"form":form})
