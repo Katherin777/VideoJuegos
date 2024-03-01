ea0a6406c003fe85b7226cfba1232896e8203529
Katherin777 <157761405+Katherin777@users.noreply.github.com>
2024-03-01 21:08:03 +0100

    Update settings.py
    delete

--- ProyectoWeb/ProyectoWeb/settings.py
+++ ProyectoWeb/ProyectoWeb/settings.py
@@ -137,7 +137,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp-mail.outlook.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "katherin_carina06@hotmail.com"
+EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 
 
